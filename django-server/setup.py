c536450533b3ef999777db26fe40d7bd6ca0d6c3
miguel <miguelkarlojuan@gmail.com>
2021-10-23 10:11:57 +0000

    modify hosts file

--- django-server/setup.py
+++ django-server/setup.py
@@ -1,7 +1,21 @@
 import os, subprocess
 
-subprocess.run(['sudo', 'apt', 'update'])
-subprocess.run(['sudo', 'apt', 'upgrade', '-y'])
-subprocess.run(['sudo', 'apt', 'install', 'python3', '-y'])
-subprocess.run(['sudo', 'apt', 'install', 'python3-pip', '-y'])
-subprocess.run(['hostnamectl', 'set-hostname', 'django-server'])
+server_name = 'django_server'
+ip_address = '10.10.10.10'
+
+# subprocess.run(['sudo', 'apt', 'update'])
+# subprocess.run(['sudo', 'apt', 'upgrade', '-y'])
+# subprocess.run(['sudo', 'apt', 'install', 'python3', '-y'])
+# subprocess.run(['sudo', 'apt', 'install', 'python3-pip', '-y'])
+# subprocess.run(['hostnamectl', 'set-hostname', 'django-server'])
+
+with open('/etc/hosts') as host_file:
+    content = host_file.readlines()
+
+content.insert(1, 'ip_address    server_name')
+
+with open('/etc/hosts', 'w') as host_file:
+    content = ''.join(content)
+    host_file.write(content)
+
+# /etc/hosts 
