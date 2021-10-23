c0c1243471ce0dd3a6b0dac01f4bb46b2bff37c4
miguel <miguelkarlojuan@gmail.com>
2021-10-23 17:19:40 +1300

    add condition

--- basic-az-env/deploy_script.py
+++ basic-az-env/deploy_script.py
@@ -1,15 +1,23 @@
 import os, subprocess, random, string
 
-tenant_id = os.environ['AZ_TENANT']
-sub_id = os.environ['AZ_SUB']
-user_id = os.environ['AZ_USER']
-user_pass = os.environ['AZ_PASS']
+tenant_id = os.environ["AZ_TENANT"]
+sub_id = os.environ["AZ_SUB"]
+user_id = os.environ["AZ_USER"]
+user_pass = os.environ["AZ_PASS"]
 
-rg_name = input('Enter the Resource Group name:\n')
-rg_location = input('Enter the Azure Region:\n')
-vm_count = int(input('How many VMs do you want to create?\n'))
-vm_base_name = input('Name the VM base name:\n')
-adm_username = input('Enter the VM Admin Username:\n')
+rg_name = input("Enter the Resource Group name:\n")
+rg_location = input("Enter the Azure Region:\n")
+
+if rg_location != "australiaeast" or rg_location != "australiasoutheast":
+    print("Only australiaeast or australiasoutheast are allowed.")
+    rg_location = input("Enter the Azure Region:\n")
+else:
+    exit
+
+vm_count = int(input("How many VMs do you want to create?\n"))
+vm_base_name = input("Name the VM base name:\n")
+adm_username = input("Enter the VM Admin Username:\n")
+
 
 # login to Azure
 subprocess.run(["az", "login", "--service-principal", "-u", user_id, "-p", user_pass, "--tenant", tenant_id]) 
@@ -19,7 +27,9 @@ subprocess.run(["az", "group", "create", "-l", rg_location, "-n", rg_name])
 
 # create the virtual machine/s
 for vm in range(vm_count):
-    random_string = ''.join(random.choices(string.ascii_lowercase, k=4))
+    random_string = "".join(random.choices(string.digits, k=4))
     vm_name = f"{vm_base_name}-{random_string}"
+
     subprocess.run(["az", "vm", "create", "-g", rg_name, "-n", vm_name, "--image", "UbuntuLTS", "--admin-username", adm_username, "--generate-ssh-keys", "--public-ip-sku", "Standard"])
+    
     print(f"{vm_name} created.")
