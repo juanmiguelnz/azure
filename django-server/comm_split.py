92ae8fd77cdebdb483991633e2974a4aecf6a7cf
miguel <miguelkarlojuan@gmail.com>
2021-10-23 08:07:11 +0000

    django setup script

---
+++ django-server/comm_split.py
@@ -0,0 +1,4 @@
+def text_split(command):
+     print(command.split())
+
+text_split('sudo apt install python3-pip')
---
+++ django-server/setup.py
@@ -0,0 +1,6 @@
+import os, subprocess
+
+subprocess.run(['sudo', 'apt', 'update'])
+subprocess.run(['sudo', 'apt', 'upgrade', '-y'])
+subprocess.run(['sudo', 'apt', 'install', 'python3', '-y'])
+subprocess.run(['sudo', 'apt', 'install', 'python3-pip', '-y'])
